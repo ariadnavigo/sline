d7630deab33dc6c2eb95bd8b88ce219af4d487f8
Ariadna Vigo <ariadna@ariadnavigo.xyz>
2021-08-01 15:33:40 +0200

    sline_test.c: showcasing how to catch EOF using sline_err

--- sline_test.c
+++ sline_test.c
@@ -12,7 +12,7 @@ int
 main(void)
 {
 	char buf[BUF_SIZE];
-	int cnt;
+	int cnt, sline_stat;
 
 	if (sline_setup(HISTORY_SIZE) < 0) {
 		fprintf(stderr, "sline: %s", sline_errmsg());
@@ -24,13 +24,15 @@ main(void)
 		/* Comment line below to use the default prompt. */
 		sline_set_prompt("%d> ", cnt++);
 
-		if (sline(buf, BUF_SIZE) < 0)
+		if ((sline_stat = sline(buf, BUF_SIZE)) < 0)
 			goto exit;
 
 		printf("Input was: %s\n", buf);
 	}
 
 exit:
+	if (sline_stat < 0 && sline_err != SLINE_ERR_EOF)
+		fprintf(stderr, "sline: %s.\n", sline_errmsg());
 	sline_end();
 
 	return 0;
